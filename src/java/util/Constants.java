07e8c28cfc1f4e893d2ded580842347c8e91b621
TijanaAsceric <tijana.asceric@gmail.com>
2014-09-22 23:44:57 +0200

    Update Constants.java

--- src/java/util/Constants.java
+++ src/java/util/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     public static final String POMOCNAO="https://api.opencorporates.com/v0.3/companies/search?q=bank";
     public static final String POMOCNAC="http://api.crunchbase.com/v/2/organizations?organization_types=company&category_uuids=68250c3a6cf586fb281e9072ae2d4fe8&user_key=da6641a8411e183da539754ab9373f2d&page=1";
     public static final String KOMPANIJE2="http://api.crunchbase.com/v/2/organization/";
-    public static final String APIKEY="?user_key=da6641a8411e183da539754ab9373f2d";
+    public static final String APIKEY="?user_key=";
     
       
       
